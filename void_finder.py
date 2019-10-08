3c306bcc0f3152be6379fd05e4226a55400d2b60
Enrique Paillas <epaillas@Enriques-MacBook-Pro.local>
2019-10-08 15:02:29 -0300

    Update

--- void_finder.py
+++ void_finder.py
@@ -3,14 +3,14 @@ from python_tools.sphericalvoids import SphericalVoids
 
 @click.command()
 @click.option('--tracers', type=str, required=True, help='File containing tracers.')
-@click.option('--randoms', type=str, help='File containing randoms.')
+@click.option('--randoms', type=str, default='', help='File containing randoms.')
 @click.option('--handle', type=str, required=True, help='Basename for the output files')
 @click.option('--is_box', type=bool, default=True, help='Is the data from a simulation box? (True or False')
 @click.option('--boss_like', type=bool, default=False, help='Is the data from BOSS/eBOSS?')
 @click.option('--ncores', type=int, default=1, help='Number of cores to use for parallel tasks.')
 @click.option('--box_size', type=float, default=1024, help='Size of the simulation box (only used if is_box is True)')
 @click.option('--steps', type=str, default='1,2,3,4', help='Which steps are to be run. (e.g. 1,2,3).')
-@click.option('--mask', type=str, help='File containing HEALPix mask of survey footprint.')
+@click.option('--mask', type=str, default='', help='File containing HEALPix mask of survey footprint.')
 @click.option('--pos_cols', type=str, default='1,2,3', help='Indices of columns where tracer positions are stored.')
 @click.option('--rvoidmax', type=float, default=100, help='Maximum void radius to search.')
 def run_spherical_voids(tracers, randoms, handle, is_box, ncores,
