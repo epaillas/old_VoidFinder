c8439ab76d08d6a275862d7a85e50d51c947a477
epaillas <epaillas@astro.puc.cl>
2019-10-29 17:27:13 -0300

    update

--- void_postprocess.py
+++ void_postprocess.py
@@ -10,7 +10,6 @@ from python_tools.voidstatistics import VoidStatistics
 @click.option('--boss_like', type=bool, default=False, help='Is the data from BOSS/eBOSS?')
 @click.option('--ncores', type=int, default=1, help='Number of cores to use for parallel tasks.')
 @click.option('--box_size', type=float, default=1024, help='Size of the simulation box (only used if is_box is True)')
-@click.option('--mask_file', type=str, help='File containing HEALPix mask of survey footprint.')
 @click.option('--pos_cols', type=str, default='1,2,3', help='Indices of columns where tracer positions are stored.')
 def postprocess_voids(voids, tracers, randoms, handle, is_box,
                       ncores, box_size, steps, boss_like, mask_file,
