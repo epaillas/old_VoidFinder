8d2670927b9c8cb341e7bf0168c654b8b1a44421
epaillas <epaillas@astro.puc.cl>
2019-10-30 13:57:30 -0300

    update

--- void_postprocess_AP.py
+++ void_postprocess_AP.py
@@ -11,12 +11,16 @@ from python_tools.aptest import VoidStatistics
 @click.option('--ncores', type=int, default=1, help='Number of cores to use for parallel tasks.')
 @click.option('--box_size', type=float, default=1024, help='Size of the simulation box (only used if is_box is True)')
 @click.option('--pos_cols', type=str, default='1,2,3', help='Indices of columns where tracer positions are stored.')
+@click.option('--omega_m', type=float, default=0.31, help='[Survey] Omega matter.')
+@click.option('--h', type=float, default=67.77, help='[Survey] Little h.')
 def postprocess_voids(voids, tracers, randoms, handle, is_box,
-                      ncores, box_size, boss_like, pos_cols):
+                      ncores, box_size, boss_like, pos_cols,
+                      omega_m, h):
 
     voids = VoidStatistics(void_file=voids, tracer_file=tracers, random_file=randoms,
                         handle=handle, is_box=is_box, box_size=box_size,
-                        ncores=ncores, boss_like=boss_like, pos_cols=pos_cols)
+                        ncores=ncores, boss_like=boss_like, pos_cols=pos_cols,
+                        omega_m=omega_m, h=h)
 
     #voids.VoidGalaxyCCF(kind='monopole')
     voids.VoidGalaxyCCF(kind='r-mu')
