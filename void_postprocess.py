b3a4e604a4f82228b0f17ccb68a1f7c59c4a3fae
epaillasv <epaillasv@gmail.com>
2019-12-12 19:08:05 -0300

    update

--- void_postprocess.py
+++ void_postprocess.py
@@ -6,11 +6,12 @@ from python_tools.voidstatistics import VoidStatistics
 @click.option('--tracers', type=str, help='File containing tracers.')
 @click.option('--randoms', type=str, help='File containing randoms.')
 @click.option('--handle', type=str, required=True, help='Basename for the output files')
-@click.option('--is_box', type=bool, default=True, help='Is the data from a simulation box? (True or False')
+@click.option('--is_box', type=bool, default=True, help='Is the data from a simulation box?')
 @click.option('--boss_like', type=bool, default=False, help='Is the data from BOSS/eBOSS?')
 @click.option('--ncores', type=int, default=1, help='Number of cores to use for parallel tasks.')
 @click.option('--box_size', type=float, default=1024, help='Size of the simulation box (only used if is_box is True)')
 @click.option('--pos_cols', type=str, default='1,2,3', help='Indices of columns where tracer positions are stored.')
+@click.option('--velocity', type=bool, default=False, help='Include velocity statistics?')
 def postprocess_voids(voids, tracers, randoms, handle, is_box,
                       ncores, box_size, boss_like, pos_cols):
 
@@ -19,7 +20,9 @@ def postprocess_voids(voids, tracers, randoms, handle, is_box,
                         ncores=ncores, boss_like=boss_like, pos_cols=pos_cols)
 
     voids.VoidGalaxyCCF(kind='r-mu')
-    voids.VoidGalaxyCCF(kind='los_velocity')
+    
+    if velocity:
+        voids.VoidGalaxyCCF(kind='los_velocity')
 
 
 if __name__ == '__main__':
