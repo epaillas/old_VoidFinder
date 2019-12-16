5315e702107d57e730d92f190c1c0baceb2be255
epaillasv <epaillasv@gmail.com>
2019-12-16 17:52:16 -0300

    update

--- SVF_box/src/vg_ccf_los_velocity.f90
+++ SVF_box/src/vg_ccf_los_velocity.f90
@@ -240,6 +240,8 @@ program vg_ccf_r_mu
       end do
     end do
 
+    print*, counter(rind)
+
     do i = 1, nrbin
       mean_vel(i) = sum(VG(:, i)) / counter(i)
       std_vel(i) = sqrt(sum((VG(:, i) - mean_vel(i))**2) / (counter(i) - 1))
@@ -250,7 +252,6 @@ program vg_ccf_r_mu
     
     open(12, file=output_den, status='unknown')
     do i = 1, nrbin
-      print*, i
       write(12, fmt='(5f10.5)') rbin(i), rbin_edges(i), rbin_edges(i + 1),&
       & mean_vel(i), std_vel(i)
     end do
