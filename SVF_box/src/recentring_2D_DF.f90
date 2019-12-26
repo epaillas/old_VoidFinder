33145b13f3ad41a473bfa9a5e39158863981ffea
epaillasv <epaillasv@gmail.com>
2019-12-26 19:51:59 -0300

    update

--- SVF_box/src/recentring_2D_DF.f90
+++ SVF_box/src/recentring_2D_DF.f90
@@ -4,12 +4,12 @@ contains
 
   subroutine linked_list(ngrid, rgrid, ll, lirst, pos_data)
     implicit none
-    integer*4 :: i, ng, ipx, ipy
-    integer*4, intent(in) :: ngrid
+    integer :: i, ng, ipx, ipy
     real*8, intent(in) :: rgrid
+    integer, intent(in) :: ngrid
     real*8, dimension(:,:), intent(in) :: pos_data
-    integer*4, dimension(:,:), intent(out) :: lirst
-    integer*4, dimension(:), intent(out) :: ll
+    integer, dimension(:,:), intent(out) :: lirst
+    integer, dimension(:), intent(out) :: ll
 
     ng = size(pos_data, dim=2)
     lirst = 0
@@ -28,12 +28,11 @@ contains
 
       if (ipx.gt.0.and.ipx.le.ngrid.and.ipy.gt.0.and.ipy.le.ngrid) then
         ll(lirst(ipx, ipy)) = i
-        lirst(ipx, ipy) = i
+        lirst(ipx, ipy) = i 
       endif
     end do
 
   end subroutine linked_list
-
 
   character(len=20) function str(k)
     implicit none
@@ -46,24 +45,28 @@ contains
 end module procedures
 
 
-PROGRAM grow_spheres
+PROGRAM recentering
   use mpi
   use procedures
   implicit none
 
   real*8 :: boxsize, delta, rgrid, rho_mean, nden
   real*8 :: px, py, disx, disy, dis
+  real*8 :: pxr, pyr, rvoidr
   real*8 :: rvoid, rwidth, rvoidmax
+  real*8 :: rnd, rnd_phi, rnd_theta, rnd_rvoid
+  real*8 :: rnd_px, rnd_py, rnd_ng, rnd_nden
   real*8 :: pi = 4.*atan(1.)
 
-  integer*4 :: ng, nc, nv, rind, nrows, ncols
-  integer*4 :: id, ierr, process_num, iargc, filenumber
-  integer*4 :: i, j, k, ii, ix, iy, ix2, iy2
-  integer*4 :: ipx, ipy, ndif, ngrid
-  integer*4, parameter :: nrbin = 1000
+  integer :: ng, nc, nv, rind, stuck, nrows, ncols
+  integer :: id, ierr, process_num, iargc, filenumber
+  integer :: i, j, k, ii, ix, iy, ix2, iy2
+  integer :: ipx, ipy, ndif, ngrid
+  integer, parameter ::  nrbin = 1000, nrc = 128
 
-  real*8, allocatable, dimension(:,:)  :: field, centres
-  real*8, dimension(nrbin) :: rbin, cum_rbin, counter
+  integer, dimension(:,:), allocatable :: field
+
+  real*8, dimension(nrbin) :: rbin, cum_rbin
 
   character(len=500) :: input_field, input_centres, output_voids
   character(len=10) :: box_char, rvoidmax_char, delta_char, ngrid_char
@@ -74,8 +77,8 @@ PROGRAM grow_spheres
   call MPI_Comm_rank(MPI_COMM_WORLD, id, ierr)
 
 
-  if (iargc() .ne. 6) then
-    if (id == 0) write(*,*) 'grow_spheres.exe: some parameters are missing.'
+  if (iargc() .ne. 7) then
+    if (id == 0) write(*,*) 'recentering.exe: some parameters are missing.'
     if (id == 0) write(*,*) ''
     if (id == 0) write(*,*) '1) input_field'
     if (id == 0) write(*,*) '2) input_centres'
@@ -83,6 +86,7 @@ PROGRAM grow_spheres
     if (id == 0) write(*,*) '4) boxsize'
     if (id == 0) write(*,*) '5) density_threshold'
     if (id == 0) write(*,*) '6) rvoidmax'
+    if (id == 0) write(*,*) '7) ngrid'
     stop
   end if
 
@@ -92,14 +96,16 @@ PROGRAM grow_spheres
   call getarg(4, box_char)
   call getarg(5, delta_char)
   call getarg(6, rvoidmax_char)
+  call getarg(7, ngrid_char)
 
   read(box_char, *) boxsize
   read(rvoidmax_char, *) rvoidmax
   read(delta_char, *) delta
+  read(ngrid_char, *) ngrid
 
 
   if (id == 0) write(*,*) '-----------------------'
-  if (id == 0) write(*,*) 'Running grow_spheres_DF.exe'
+  if (id == 0) write(*,*) 'Running recentering.exe'
   if (id == 0) write(*,*) 'Input parameters:'
   if (id == 0) write(*,*) ''
   if (id == 0) write(*,*) 'mpi_processes: ', process_num
@@ -109,6 +115,9 @@ PROGRAM grow_spheres
   if (id == 0) write(*,*) 'boxsize: ', trim(box_char), ' Mpc/h'
   if (id == 0) write(*,*) 'rvoidmax: ', trim(rvoidmax_char), ' Mpc/h'
   if (id == 0) write(*,*) 'density_threshold: ', trim(delta_char), ' * rho_mean'
+  if (id == 0) write(*,*) 'random_centres: ', nrc
+  if (id == 0) write(*,*) 'ngrid: ', ngrid, ' Mpc/h'
+  if (id == 0) write(*,*) ''
 
   if (process_num .gt. 1) then
     output_voids = trim(output_voids) // '.' // trim(str(id))
@@ -117,55 +126,66 @@ PROGRAM grow_spheres
   open(10, file=input_field, status='old', form='unformatted')
   read(10) nrows
   read(10) ncols
+  print*, nrows, ncols
+  stop
   allocate(field(ncols, nrows))
   read(10) field
-  ngrid = nrows
-  field = field + 1
+  close(10)
 
-  open(11, file=input_centres, status='old', form='unformatted')
-  read(11) nrows
-  ncols = 2 ! need to rerun centres
-  allocate(centres(ncols, nrows))
-  read(11) centres
-  nc = nrows
+  open(11, file=input_centres, status='old')
+  nc = 0
+  do
+    read(11, *, end=11)
+    nc = nc + 1
+  end do
+  11 rewind(11)
+  if (id == 0) write(*,*) 'n_centres: ', nc
+  if (id == 0) write(*,*) ''
 
-  if (id == 0) write(*,*) 'ncentres: ', nc
-  if (id == 0) write(*,*) 'xmin, xmax: ', minval(centres(1,:)), maxval(centres(1,:))
-
+  
   rho_mean = sum(field) / size(field)
   rgrid = boxsize / ngrid
-  ndif = int(rvoidmax / rgrid + 1)
+  ndif = int(rvoidmax / rgrid + 1.)
   rwidth = rvoidmax / nrbin
-
-  if (id == 0) write(*,*) 'rho_mean: ', rho_mean
-  if (id == 0) write(*,*) 'rgrid: ', rgrid, ' Mpc/h'
-  if (id == 0) write(*,*) 'rwidth: ', rwidth, ' Mpc/h'
-  if (id == 0) write(*,*) 'ndif: ', ndif
-  if (id == 0) write(*,*) ''
 
   filenumber = id + 20
   open(filenumber, file=output_voids, status='unknown')
   nv = 0
   do i = 1, nc
+    read(11,*) px, py, rvoid, ng, nden
 
-    px = centres(1, i)
-    py = centres(2, i)
-    counter = 0
-
+    pxr = px
+    pyr = py
+    rvoidr = rvoid
 
     if(mod(i, process_num) .eq. id) then
 
+      stuck = 0
+      do j = 1, nrc
         rbin = 0
         cum_rbin = 0
 
-        ipx = int(px / rgrid + 1)
-        ipy = int(py / rgrid + 1)
+        call random_number(rnd)
+        rnd_phi = rnd * 2 * pi
+
+        rnd_px = rvoid/4 * cos(rnd_phi) + px
+        rnd_py = rvoid/4 * sin(rnd_phi) + py
+
+        if (sqrt((rnd_px-pxr)**2 + (rnd_py-pyr)**2)&
+        & .gt. rvoidr) cycle
+
+        if (rnd_px .lt. 0) rnd_px = rnd_px + boxsize
+        if (rnd_px .gt. boxsize) rnd_px = rnd_px - boxsize
+        if (rnd_py .lt. 0) rnd_py = rnd_py + boxsize
+        if (rnd_py .gt. boxsize) rnd_py = rnd_py - boxsize
+
+        ipx = int(rnd_px / rgrid + 1.)
+        ipy = int(rnd_py / rgrid + 1.)
 
         do ix = ipx - ndif, ipx + ndif, 1
           do iy = ipy - ndif, ipy + ndif, 1
 
-            if (sqrt(real((ix - ipx) ** 2 + (iy - ipy)** 2))&
-            & .gt. ndif + 1) cycle
+            if (sqrt(real((ix-ipx)**2 +(iy-ipy)**2)).gt.ndif+1) cycle
 
             ix2 = ix
             iy2 = iy
@@ -188,11 +208,12 @@ PROGRAM grow_spheres
             if (dis .lt. rvoidmax) then
               rind = int(dis / rwidth + 1)
               rbin(rind) = rbin(rind) + field(ix2, iy2)
-              counter(rind) = counter(rind) + 1
             end if
 
           end do
         end do
+
+        stuck = stuck + 1
 
         cum_rbin(1) = rbin(1)
         do ii = 2, nrbin
@@ -200,15 +221,24 @@ PROGRAM grow_spheres
         end do
 
         do ii = nrbin, 1, -1
-          rvoid = rwidth * ii
-          nden = cum_rbin(ii) / sum(counter(1:ii))
-          if (nden .lt. delta * rho_mean) then
-            nv = nv + 1
-            write(filenumber, '(4F10.3)') &
-            px, py, rvoid, nden / rho_mean
+          rnd_rvoid = rwidth * ii
+          rnd_nden = cum_rbin(ii)
+          if (rnd_nden .lt. delta * rho_mean .and. rnd_rvoid .gt. rvoid) then
+            rvoid = rnd_rvoid
+            px = rnd_px
+            py = rnd_py
+            nden = rnd_nden / rho_mean
+            stuck = 0
             exit
           end if
         end do
+
+        if (stuck .gt. 64) exit
+
+      end do
+
+      write(filenumber, '(4F10.3)') px, py, rvoid, nden
+
     end if
   end do
 
@@ -216,8 +246,7 @@ PROGRAM grow_spheres
 
   if (id == 0) write(*,*) ''
   if (id == 0) write(*,*) ''
-  if (id == 0) write(*,*) nv, ' voids found by rank 0.'
 
   call MPI_Finalize(ierr)
 
-end PROGRAM grow_spheres
+end PROGRAM recentering
