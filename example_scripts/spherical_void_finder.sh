c0671f20dcd829c73a28e57e61788531b19007ab
epaillasv <epaillasv@gmail.com>
2020-01-27 17:11:59 -0300

    update

--- example_scripts/spherical_void_finder.sh
+++ example_scripts/spherical_void_finder.sh
@@ -10,6 +10,7 @@ steps='1,2,3,4'
 pos_cols='0,1,2'
 rvoidmax=100
 skip_header=0
+has_velocity=False
 
 #-------- Periodic box settings --------
 box_size=1000
@@ -37,4 +38,5 @@ python $HOME/code/void_finder/void_finder.py \
 --boss_like "$boss_like" \
 --zmin "$zmin" \
 --zmax "$zmax" \
---skip_header "$skip_header"
+--skip_header "$skip_header" \
+--has_velocity "$has_velocity"
